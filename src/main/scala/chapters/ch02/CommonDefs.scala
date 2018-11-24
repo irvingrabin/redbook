84bb27f033fd800628846af8ff9c35b53c735b67
Irving Rabin <ii6rr5@gmail.com>
2018-11-24 07:39:16 -0800

    Refactoring chapter 2

---
+++ src/main/scala/chapters/ch02/CommonDefs.scala
@@ -0,0 +1,27 @@
+package chapters.ch02
+
+object CommonDefs {
+  def fib(n: Int): Int = {
+    def _fib(n: Int, acc: Int): Int = {
+      if (n < 2) acc else _fib(n - 1, acc * n)
+    }
+    _fib(n, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def _isSorted(as: Array[A], ordered: (A, A) => Boolean, pos: Int): Boolean = {
+      if (pos >= as.length -1) true
+      else if (!ordered(as(pos), as(pos+1))) false
+      else _isSorted(as, ordered, pos+1)
+    }
+    _isSorted(as, ordered, 0)
+  }
+
+  def ascInt(a: Int, b: Int): Boolean = a <= b
+  def descInt(a: Int, b: Int): Boolean = a >= b
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => f(a, _)
+  def uncurry[A, B, C](f:A => B => C): (A, B) => C = f(_)(_)
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
+}
---
+++ src/test/scala/chapters/ch02/Chapter02Spec.scala
@@ -0,0 +1,53 @@
+package chapters.ch02
+
+import chapters.ch02.CommonDefs.{
+  fib, ascInt, descInt, isSorted, curry, uncurry, compose}
+import org.scalatest.{FlatSpec, Matchers}
+
+class Chapter02Spec extends FlatSpec with Matchers {
+
+  it should "run test 02_01" in {
+    fib(0) shouldBe 1
+    fib(3) shouldBe 6
+    fib(6) shouldBe 720
+  }
+
+  it should "run test 02_02" in {
+    isSorted(Array(4, 6, 4), ascInt(_, _)) shouldBe false
+    isSorted(Array(4, 6, 4), descInt(_, _)) shouldBe false
+
+    isSorted(Array(4, 6, 6), ascInt(_, _)) shouldBe true
+    isSorted(Array(4, 6, 6), descInt(_, _)) shouldBe false
+
+    isSorted(Array(6, 6, 4), ascInt(_, _)) shouldBe false
+    isSorted(Array(6, 6, 4), descInt(_, _)) shouldBe true
+
+    isSorted(Array(4), ascInt(_, _)) shouldBe true
+    isSorted(Array(4), descInt(_, _)) shouldBe true
+
+    isSorted(Array[Int](), ascInt(_, _)) shouldBe true
+    isSorted(Array[Int](), descInt(_, _)) shouldBe true
+  }
+
+  it should "run test 02_03" in {
+    val add = (a: Int, b: Int) => a + b
+    val multiply = (a: Int, b: Int) => a * b
+
+    curry(add)(5)(6) shouldEqual add(5, 6)
+    curry(multiply)(5)(6) shouldEqual multiply(5, 6)
+  }
+
+  it should "run test 02_04" in {
+    val addCurry = (a: Int) => (b: Int) => a + b
+    val multiplyCurry = (a: Int) => (b: Int) => a * b
+    assert(uncurry(addCurry)(5, 6) == addCurry(5)(6))
+    assert(uncurry(multiplyCurry)(5, 6) == multiplyCurry(5)(6))
+  }
+
+  it should "run test 02_05" in {
+    val f1 = (a: Int) => a + 5
+    val f2 = (a: Int) => a * 3
+    assert(compose(f1, f2)(5) == 20)
+    assert(compose(f2, f1)(5) == 30)
+  }
+}
