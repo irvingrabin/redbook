d4a640a60f14b5fef993eb1a64d7cf6c08907b83
Irving Rabin <ii6rr5@gmail.com>
2018-12-03 07:32:02 -0800

    Save some streams

--- src/main/scala/chapters/ch05/Stream.scala
+++ src/main/scala/chapters/ch05/Stream.scala
@@ -12,14 +12,40 @@ sealed trait Stream[+A] {
     case Cons(h, t) => ListCons(h(), t().toList)
   }
   override def toString: String = toList.toString
-  def take(n: Int): Seq[A] = this match {
-    case Cons(h, t) if n > 0 => h() +: t().take(n - 1)
-    case _                   => Seq.empty
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n-1))
+    case _                   => Empty
   }
   def drop(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 0 => t().drop(n - 1)
     case _                   => this
   }
+  def takeWhile(f: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) => {
+      val hv = h()
+      if (f(hv)) Cons(h, () => t().takeWhile(f)) else Empty
+    }
+    case _          => Empty
+  }
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => {
+      val hv = h()
+      if (p(hv)) t().forAll(p) else false
+    }
+    case Empty      => true
+  }
+  def foldRight[B](z: B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => t().foldRight(f(h(),z))(f)
+    case Empty      => z
+  }
+//  def takeWhile2(f: A=> Boolean): Stream[A] = foldRight(Empty[A])((h, v) => v match {
+//    case Cons(h, t) => {
+//      val hv = h()
+//      if (f(hv)) t().foldRight(h, f)) else Empty
+//    }
+//    case _ => Empty
+//  })
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
--- src/test/scala/chapters/ch05/Chapter_05_Spec.scala
+++ src/test/scala/chapters/ch05/Chapter_05_Spec.scala
@@ -10,18 +10,27 @@ class Chapter_05_Spec  extends FlatSpec with Matchers {
 
   it should "05_02 implement take(n) and drop(n)" in {
     val s1 = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    s1.take(5) shouldEqual Seq(1, 2, 3, 4, 5)
+    s1.take(5) shouldBe a [Stream[_]]
+    s1.take(5).toList shouldEqual List(1, 2, 3, 4, 5)
     s1.drop(5) shouldBe a [Stream[_]]
     s1.drop(5).toList shouldEqual List(6, 7, 8, 9)
   }
 
-  it should "05_03 " in {
+  it should "05_03 implement takewhile" in {
+    Stream(1, 2, 3, 4, 5, 6, 7).takeWhile(_ < 5) shouldBe a [Stream[_]]
+    Stream(1, 2, 3, 4, 5, 6, 7).takeWhile(_ < 5).toList shouldBe List(1, 2, 3, 4)
+    Stream(1, 2, 3, 4, 5, 6, 7).takeWhile(_ > 5) shouldBe a [Stream[_]]
+    Stream(1, 2, 3, 4, 5, 6, 7).takeWhile(_ > 5).toList shouldBe List()
   }
 
-  it should "05_04 " in {
+  it should "05_04 implement forAll" in {
+    Stream(2, 4, 6, 8).forAll(_ % 2 == 0) shouldBe true
+    Stream(2, 4, 5, 6, 8).forAll(_ % 2 == 0) shouldBe false
+    Stream[Int]().forAll(_ * 0 == 1) shouldBe true
   }
 
   it should "05_05 " in {
+    Stream(1, 2, 3, 4, 5).foldRight(1)(_ * _) shouldBe 120
   }
 
   it should "05_06 " in {
